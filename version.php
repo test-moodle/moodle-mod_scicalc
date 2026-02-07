613d3dce29bf94cbcf53e8e24b81645439f58a6c
Eduardo Kraus <kraus@eduardokraus.com>
2026-02-07 12:03:09 -0300

    V 1.0.1

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
+$plugin->version = 2026020701;
+$plugin->release = "1.0.1";
 $plugin->component = "mod_scicalc";
-$plugin->version = 2026020700;
 $plugin->requires = 2022041900; // Moodle 4.0.
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = "1.0.0";
